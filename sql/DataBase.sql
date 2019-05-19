5c69825c3fe69ce7f5b08f6d8f5c0fdb2b7d0c1d
Fabrício Almeida <fabricio@automatizasistemas.com.br>
2019-05-19 14:22:49 -0300

    Example database tables mysql

---
+++ sql/DataBase.sql
@@ -0,0 +1,23 @@
+﻿CREATE DATABASE `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+
+
+CREATE TABLE  `test`.`supplier` (
+  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+
+CREATE TABLE  `test`.`product` (
+  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) NOT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  `Quantity` int(10) unsigned NOT NULL,
+  `SupplierId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+
+
+
